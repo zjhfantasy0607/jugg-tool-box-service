eaa3b42b9b517cff80b423a8c2bfacf4c4566e79
zjhong <262559158@qq.com>
2024-05-20 16:14:19 +0800

    第二次的提交

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.POST("/api/auth/register", func(c *gin.Context) {
+		// 获取参数
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+
+		// 数据验证
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+			return
+		}
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+			return
+		}
+		if len(name) == 0 {
+			name = RandomString(10)
+		}
+
+		log.Println(name, telephone, password)
+
+		// 判断手机号是否存在
+
+		// 创建用户
+
+		// 返回结果
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "注册成功",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	panic(r.Run()) // listen and serve on 0.0.0.0:8080
+}
+
+func RandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
